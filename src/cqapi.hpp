99301673cf545f1de26f06a84e5e50894972c92f
weak_ptr <weak_ptr@outlook.com>
2020-02-05 17:25:17 +0800

    fix: 处理多线程崩溃的问题(events.cpp)；用clang-format对代码格式化；

--- src/cqapi.hpp
+++ src/cqapi.hpp
@@ -13,52 +13,86 @@ bool add_initializer(std::function<void(HMODULE)> fn);
 void cqapi_initialize();
 
 // Message
-CQ_IMPORT_API_DECLARE(int32_t, sendPrivateMsg, int32_t auth_code, int64_t qq, const char* msg)
-CQ_IMPORT_API_DECLARE(int32_t, sendGroupMsg, int32_t auth_code, int64_t group_id, const char* msg)
-CQ_IMPORT_API_DECLARE(int32_t, sendDiscussMsg, int32_t auth_code, int64_t discuss_id, const char* msg)
+CQ_IMPORT_API_DECLARE(int32_t, sendPrivateMsg, int32_t auth_code, int64_t qq,
+                      const char *msg)
+CQ_IMPORT_API_DECLARE(int32_t, sendGroupMsg, int32_t auth_code,
+                      int64_t group_id, const char *msg)
+CQ_IMPORT_API_DECLARE(int32_t, sendDiscussMsg, int32_t auth_code,
+                      int64_t discuss_id, const char *msg)
 CQ_IMPORT_API_DECLARE(int32_t, deleteMsg, int32_t auth_code, int64_t msg_id)
 
 // Friend Operation
 CQ_IMPORT_API_DECLARE(int32_t, sendLike, int32_t auth_code, int64_t qq)
-CQ_IMPORT_API_DECLARE(int32_t, sendLikeV2, int32_t auth_code, int64_t qq, int32_t times)
+CQ_IMPORT_API_DECLARE(int32_t, sendLikeV2, int32_t auth_code, int64_t qq,
+                      int32_t times)
 
 // Group Operation
-CQ_IMPORT_API_DECLARE(int32_t, setGroupKick, int32_t auth_code, int64_t group_id, int64_t qq, cq_bool_t reject_add_request)
-CQ_IMPORT_API_DECLARE(int32_t, setGroupBan, int32_t auth_code, int64_t group_id, int64_t qq, int64_t duration)
-CQ_IMPORT_API_DECLARE(int32_t, setGroupAnonymousBan, int32_t auth_code, int64_t group_id, const char* anonymous, int64_t duration)
-CQ_IMPORT_API_DECLARE(int32_t, setGroupWholeBan, int32_t auth_code, int64_t group_id, cq_bool_t enable)
-CQ_IMPORT_API_DECLARE(int32_t, setGroupAdmin, int32_t auth_code, int64_t group_id, int64_t qq, cq_bool_t set)
-CQ_IMPORT_API_DECLARE(int32_t, setGroupAnonymous, int32_t auth_code, int64_t group_id, cq_bool_t enable)
-CQ_IMPORT_API_DECLARE(int32_t, setGroupCard, int32_t auth_code, int64_t group_id, int64_t qq, const char* new_card)
-CQ_IMPORT_API_DECLARE(int32_t, setGroupLeave, int32_t auth_code, int64_t group_id, cq_bool_t is_dismiss)
-CQ_IMPORT_API_DECLARE(int32_t, setGroupSpecialTitle, int32_t auth_code, int64_t group_id, int64_t qq, const char* new_special_title, int64_t duration)
-CQ_IMPORT_API_DECLARE(int32_t, setDiscussLeave, int32_t auth_code, int64_t discuss_id)
+CQ_IMPORT_API_DECLARE(int32_t, setGroupKick, int32_t auth_code,
+                      int64_t group_id, int64_t qq,
+                      cq_bool_t reject_add_request)
+CQ_IMPORT_API_DECLARE(int32_t, setGroupBan, int32_t auth_code, int64_t group_id,
+                      int64_t qq, int64_t duration)
+CQ_IMPORT_API_DECLARE(int32_t, setGroupAnonymousBan, int32_t auth_code,
+                      int64_t group_id, const char *anonymous, int64_t duration)
+CQ_IMPORT_API_DECLARE(int32_t, setGroupWholeBan, int32_t auth_code,
+                      int64_t group_id, cq_bool_t enable)
+CQ_IMPORT_API_DECLARE(int32_t, setGroupAdmin, int32_t auth_code,
+                      int64_t group_id, int64_t qq, cq_bool_t set)
+CQ_IMPORT_API_DECLARE(int32_t, setGroupAnonymous, int32_t auth_code,
+                      int64_t group_id, cq_bool_t enable)
+CQ_IMPORT_API_DECLARE(int32_t, setGroupCard, int32_t auth_code,
+                      int64_t group_id, int64_t qq, const char *new_card)
+CQ_IMPORT_API_DECLARE(int32_t, setGroupLeave, int32_t auth_code,
+                      int64_t group_id, cq_bool_t is_dismiss)
+CQ_IMPORT_API_DECLARE(int32_t, setGroupSpecialTitle, int32_t auth_code,
+                      int64_t group_id, int64_t qq,
+                      const char *new_special_title, int64_t duration)
+CQ_IMPORT_API_DECLARE(int32_t, setDiscussLeave, int32_t auth_code,
+                      int64_t discuss_id)
 
 // Request
-CQ_IMPORT_API_DECLARE(int32_t, setFriendAddRequest, int32_t auth_code, const char* response_flag, int32_t response_operation, const char* remark)
-CQ_IMPORT_API_DECLARE(int32_t, setGroupAddRequest, int32_t auth_code, const char* response_flag, int32_t request_type, int32_t response_operation)
-CQ_IMPORT_API_DECLARE(int32_t, setGroupAddRequestV2, int32_t auth_code, const char* response_flag, int32_t request_type, int32_t response_operation, const char* reason)
+CQ_IMPORT_API_DECLARE(int32_t, setFriendAddRequest, int32_t auth_code,
+                      const char *response_flag, int32_t response_operation,
+                      const char *remark)
+CQ_IMPORT_API_DECLARE(int32_t, setGroupAddRequest, int32_t auth_code,
+                      const char *response_flag, int32_t request_type,
+                      int32_t response_operation)
+CQ_IMPORT_API_DECLARE(int32_t, setGroupAddRequestV2, int32_t auth_code,
+                      const char *response_flag, int32_t request_type,
+                      int32_t response_operation, const char *reason)
 
 // QQ Information
 CQ_IMPORT_API_DECLARE(int64_t, getLoginQQ, int32_t auth_code)
-CQ_IMPORT_API_DECLARE(const char*, getLoginNick, int32_t auth_code)
-CQ_IMPORT_API_DECLARE(const char*, getStrangerInfo, int32_t auth_code, int64_t qq, cq_bool_t no_cache)
-CQ_IMPORT_API_DECLARE(const char*, getFriendList, int32_t auth_code, cq_bool_t reserved)
-CQ_IMPORT_API_DECLARE(const char*, getGroupList, int32_t auth_code)
-CQ_IMPORT_API_DECLARE(const char*, getGroupInfo, int32_t auth_code, int64_t group_id, cq_bool_t no_cache)
-CQ_IMPORT_API_DECLARE(const char*, getGroupMemberList, int32_t auth_code, int64_t group_id)
-CQ_IMPORT_API_DECLARE(const char*, getGroupMemberInfoV2, int32_t auth_code, int64_t group_id, int64_t qq, cq_bool_t no_cache)
+CQ_IMPORT_API_DECLARE(const char *, getLoginNick, int32_t auth_code)
+CQ_IMPORT_API_DECLARE(const char *, getStrangerInfo, int32_t auth_code,
+                      int64_t qq, cq_bool_t no_cache)
+CQ_IMPORT_API_DECLARE(const char *, getFriendList, int32_t auth_code,
+                      cq_bool_t reserved)
+CQ_IMPORT_API_DECLARE(const char *, getGroupList, int32_t auth_code)
+CQ_IMPORT_API_DECLARE(const char *, getGroupInfo, int32_t auth_code,
+                      int64_t group_id, cq_bool_t no_cache)
+CQ_IMPORT_API_DECLARE(const char *, getGroupMemberList, int32_t auth_code,
+                      int64_t group_id)
+CQ_IMPORT_API_DECLARE(const char *, getGroupMemberInfoV2, int32_t auth_code,
+                      int64_t group_id, int64_t qq, cq_bool_t no_cache)
 
 // CoolQ
-CQ_IMPORT_API_DECLARE(const char*, getCookies, int32_t auth_code)
-CQ_IMPORT_API_DECLARE(const char*, getCookiesV2, int32_t auth_code, const char* domain)
+CQ_IMPORT_API_DECLARE(const char *, getCookies, int32_t auth_code)
+CQ_IMPORT_API_DECLARE(const char *, getCookiesV2, int32_t auth_code,
+                      const char *domain)
 CQ_IMPORT_API_DECLARE(int32_t, getCsrfToken, int32_t auth_code)
-CQ_IMPORT_API_DECLARE(const char*, getAppDirectory, int32_t auth_code)
-CQ_IMPORT_API_DECLARE(const char*, getRecord, int32_t auth_code, const char* file, const char* out_format)
-CQ_IMPORT_API_DECLARE(const char*, getRecordV2, int32_t auth_code, const char* file, const char* out_format)
-CQ_IMPORT_API_DECLARE(const char*, getImage, int32_t auth_code, const char* file)
+CQ_IMPORT_API_DECLARE(const char *, getAppDirectory, int32_t auth_code)
+CQ_IMPORT_API_DECLARE(const char *, getRecord, int32_t auth_code,
+                      const char *file, const char *out_format)
+CQ_IMPORT_API_DECLARE(const char *, getRecordV2, int32_t auth_code,
+                      const char *file, const char *out_format)
+CQ_IMPORT_API_DECLARE(const char *, getImage, int32_t auth_code,
+                      const char *file)
 CQ_IMPORT_API_DECLARE(int, canSendImage, int32_t auth_code)
 CQ_IMPORT_API_DECLARE(int, canSendRecord, int32_t auth_code)
-CQ_IMPORT_API_DECLARE(int32_t, addLog, int32_t auth_code, int32_t log_level, const char* category, const char* log_msg)
-CQ_IMPORT_API_DECLARE(int32_t, setFatal, int32_t auth_code, const char* error_info)
-CQ_IMPORT_API_DECLARE(int32_t, setRestart, int32_t auth_code) // currently ineffective
+CQ_IMPORT_API_DECLARE(int32_t, addLog, int32_t auth_code, int32_t log_level,
+                      const char *category, const char *log_msg)
+CQ_IMPORT_API_DECLARE(int32_t, setFatal, int32_t auth_code,
+                      const char *error_info)
+CQ_IMPORT_API_DECLARE(int32_t, setRestart,
+                      int32_t auth_code) // currently ineffective
