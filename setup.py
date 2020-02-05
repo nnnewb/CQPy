9d32676af4ce6a4a1acdb4895bb3b8ba750c4564
weak_ptr <weak_ptr@outlook.com>
2020-02-05 17:26:04 +0800

    misc: 版本号同步更新

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    README = f.read()
+
 setup(
     name="CQPy",
-    version="0.1.0",
+    version="0.1.1",
     author="weak_ptr",
     author_email="weak_ptr@outlook.com",
+    description='CQPy is lightweight CoolQ Python SDK for Python 3.7+',
+    long_description=README,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
 )
